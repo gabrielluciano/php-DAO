bfc761c0872b50e4be3894dd1b027cb00e027204
Gabriel Luciano <66069864+gabrielluciano@users.noreply.github.com>
2020-09-02 15:24:21 -0300

    Criada a classe Usuario

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+// $sql = new Sql();
+// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+$root = new Usuario();
 
-echo json_encode($usuarios);
+$root->loadById(3);
+
+echo $root;
 
 ?>
